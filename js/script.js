86d55fe09db5a2034b872f2e99eddb1a7ec19449
mai_b <maim181@outlook.com>
2023-03-04 00:50:55 +0000

    initial commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+let tripInfoButton = document.querySelector(".trip-info");
+let dailyBudget = document.querySelector(".daily-budget");
+
+let tripInfo = function () {
+  let totalBudget = Number(prompt("What is your total budget?"));
+  let accommodation = Number(prompt("What are your accommodation costs?"));
+  let numDays = Number(prompt("How many days does your trip last?"));
+
+  calculateDailyBudget(totalBudget, accommodation, numDays);
+};
+
+let calculateDailyBudget = function (totalBudget, accommodation, numDays) {
+  let daily = (totalBudget - accommodation) / numDays.toFixed(2);
+  dailyBudget.innerText = `You can spend $${daily} a day on fodd and fun.`;
+};
+
+tripInfoButton.addEventListener("click", tripInfo);
